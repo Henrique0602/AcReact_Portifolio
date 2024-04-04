fdcc4fcadd383d0881243b91e785720e88075a8b
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-04 08:35:08 -0300

    Portifolio

---
+++ Ac1Portifolio/src/Componentes/Header/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+    return(
+
+    <header>
+       <div className="banner" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <h1>Bem-Vindo ao nosso Portfólio</h1>
+    </div>
+    </header>
+
+
+        
+    )
+}
+
+export default Header
---
+++ Ac1Portifolio/src/Componentes/Main/Card.jsx
@@ -0,0 +1,17 @@
+function Card(){
+    return(
+
+    <main>
+      return (
+            <div className="card">
+                {nome}
+            </div>
+        );
+    </main>
+
+
+        
+    )
+}
+
+export default Card
